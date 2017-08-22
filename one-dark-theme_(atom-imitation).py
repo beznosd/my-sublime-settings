42b21a25e74963758c689f7539290feca31401ce
Dima Beznos <beznos.d@gmail.com>
2017-08-23 00:58:10 +0300

    Create one-dark-theme_(atom-imitation).py

---
+++ one-dark-theme_(atom-imitation).py
@@ -0,0 +1,31 @@
+{
+	"always_show_minimap_viewport": true,
+	"caret_extra_bottom": 1,
+	"caret_extra_top": 2,
+	"caret_extra_width": 1,
+	"caret_style": "blink",
+	"color_scheme": "Packages/User/SublimeLinter/One Dark (SL).tmTheme",
+	"font_size": 15,
+	"highlight_line": true,
+	"ignored_packages":
+	[
+		"Vintage"
+	],
+	"line_padding_bottom": 2,
+	"line_padding_top": 2,
+	"margin": 0,
+	"move_to_limit_on_up_down": true,
+	"open_files_in_new_window": false,
+	"overlay_scroll_bars": "enabled",
+	"remember_open_files": true,
+	"show_definitions": false,
+	"show_encoding": true,
+	"show_full_path": true,
+	"show_line_endings": true,
+	"show_tabs_dropdown": false,
+	"tab_size": 2,
+	"theme": "One Dark.sublime-theme",
+	"translate_tabs_to_spaces": true,
+	"tree_animation_enabled": true,
+	"use_simple_full_screen": true
+}
